3df86df8ba6171a0c2f550dc8c55c9747c00ca63
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2020-08-10 01:38:48 +0430

    add script sharkman and make it trigger and add rigidbody and boxCollider

---
+++ Assets/Game/Scripts/SharkMan.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkMan : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _weaponBuy;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                player player = other.GetComponent<player>();
+                if (player != null)
+                {
+                    if (player.coinCount > 0)
+                    {
+                        player.coinCount--;
+                        AudioSource.PlayClipAtPoint(_weaponBuy, Camera.main.transform.position, 1f);
+                        UiManager ui = GameObject.Find("Canvas").GetComponent<UiManager>();
+                        if (ui != null)
+                        {
+                            ui.showCoin(player.coinCount);
+                        }
+                    }
+                    else
+                    {
+                        print("Siktir");
+                    }
+                }
+            }
+        }
+    }
+}
